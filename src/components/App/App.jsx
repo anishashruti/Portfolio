90603db096cb4b3505c3ad9e9f9a4bbf2b951bcf
Anisha Shruti A.T <anishashruti.at@gmail.com>
2021-07-01 20:24:48 +0530

    Home page routing done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,16 +7,22 @@ import Projects from './../Projects/Projects';
 import Skills from './../Skills/Skills';
 import Contact from './../Contact/Contact';
 
+import NavBar from '../NavBar/NavBar';
+import GoHome from '../GoHome/GoHome';
+
 const App = () => {
     return (
         <div>
+
             <BrowserRouter>
+            <NavBar />
                 <div>
                 <Route path='/' exact component={Home} />
                     <Route path='/about' exact component={About} />
                     <Route path='/projects' exact component={Projects} />
                     <Route path='/skills' exact component={Skills} />
                     <Route path='/contact' exact component={Contact} />
+                    <GoHome />
                 </div>
             </BrowserRouter>
         </div>
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { slide as Menu } from 'react-burger-menu'
+import {Link , withRouter} from 'react-router-dom';
+import './NavBar.css';
+import im from "./../../images/A-logo.png"
+
+ class NavBar extends Component {
+    render() {
+        const { location } = this.props;
+        console.log(location);
+        const homeClass = location.pathname === '/' ? 'active-item' : '';
+        const aboutClass = location.pathname === '/about' ? 'active-item' : '';
+        const projectsClass =
+            location.pathname === '/projects' ? 'active-item' : '';
+        const skillsClass =
+            location.pathname === '/skills' ? 'active-item' : '';
+        const contactClass =
+            location.pathname === '/contact' ? 'active-item' : '';
+        return (
+            <Menu >
+                <img className="Image" src={im} alt="logo"></img>
+                <Link id="home" className={`menu-item ${homeClass}`} to="/">Home</Link>
+                <Link id="about" className={`menu-item ${aboutClass}`} to="/about">About</Link>
+                <Link id="skills" className={`menu-item ${skillsClass}`} to="/skills">Skills</Link>
+                <Link id="projects" className={`menu-item ${projectsClass}`} to="/projects">Projects</Link>
+                <Link id="contact" className={`menu-item ${contactClass}`} to="/contact">Contact</Link>
+            </Menu>
+        );
+    }
+}
+
+export default withRouter(NavBar);
